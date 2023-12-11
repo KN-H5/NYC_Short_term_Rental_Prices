16fc6bcca963db666f0140bb94284cb644b8b153
Huy Hoang <huy2000kudo@gmail.com>
2023-12-11 17:29:05 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 
-import yaml
 import mlflow
 import tempfile
 import os
@@ -8,52 +7,20 @@ import wandb
 import hydra
 from omegaconf import DictConfig
 
-
 _steps = [
     "download",
     "basic_cleaning",
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    "test_regression_model"
 ]
 
-def get_valid_cwd():
-    """
-    Get the current working dir.
-    The current working dir can be not valid: for instance when I delete a parent dir or when I
-    git checkout a branch which does not have the current dir. In this case we return the current
-    invalid dir (because this is what the OS thinks) but we display a warning.
-    """
-    import os
-
-    try:
-        cwd = os.getcwd()
-    except FileNotFoundError:
-        cwd = os.getenv('PWD')  # This is where the OS thinks we are.
-        parts = cwd.split(os.sep)
-        up = cwd
-        while parts and not os.path.exists(up):
-            parts.pop()
-            up = os.sep.join(parts)
-        try:
-            os.chdir(up)
-        except Exception:
-            print('Your current directory is invalid!')
-            exit(1)
-        print('Your current directory is invalid!\n'
-                      'Closest valid parent directory: {}'.format(up))
-    return cwd
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
-def go(_steps: DictConfig):
+def go(config: DictConfig):
 
-    with open("./config.yaml", "r") as file:
-                config = yaml.safe_load(file)
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
@@ -70,6 +37,7 @@ def go(_steps: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
+                version='main',
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
@@ -80,13 +48,13 @@ def go(_steps: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                os.path.join(get_valid_cwd(), "src", "basic_cleaning"),
+                os.path.join(hydra.utils.get_original_cwd(),
+                             "src", "basic_cleaning"),
                 "main",
-                version='main',
                 parameters={
                     "input_artifact": "sample.csv:latest",
-                    "output_artifact": "clean_sample.csv",
-                    "output_type": "clean_sample",
+                    "output_artifact": "clean_data.csv",
+                    "output_type": "clean_data",
                     "output_description": "Data with outliers and null values removed",
                     "min_price": config['etl']['min_price'],
                     "max_price": config['etl']['max_price']
@@ -95,27 +63,30 @@ def go(_steps: DictConfig):
 
         if "data_check" in active_steps:
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
-                "main",
+                os.path.join(hydra.utils.get_original_cwd(),
+                             'src', 'data_check'),
+                'main',
                 parameters={
-                    "csv": "clean_sample.csv:latest",
-                    "ref": "clean_sample.csv:reference",
-                    "kl_threshold": config["data_check"]["kl_threshold"],
-                    "min_price": config['etl']['min_price'],
-                    "max_price": config['etl']['max_price']
-                }
+                    'csv': 'clean_sample.csv:latest',
+                    'ref': 'clean_sample.csv:reference',
+                    'kl_threshold': config['data_check']['kl_threshold'],
+                    'min_price': config['etl']['min_price'],
+                    'max_price': config['etl']['max_price']
+                },
             )
 
         if "data_split" in active_steps:
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/train_val_test_split",
-                "main",
+                'main',
+                version='main',
                 parameters={
-                    "input": "clean_data.csv:latest",
-                    "test_size": config["modeling"]["test_size"],
-                    "random_seed": config["modeling"]["random_seed"],
-                    "stratify_by": config["modeling"]["stratify_by"]
-                }
+                    'input': 'clean_sample.csv:latest',
+                    'test_size': config['modeling']['test_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by']
+                },
+
             )
 
         if "train_random_forest" in active_steps:
@@ -123,35 +94,39 @@ def go(_steps: DictConfig):
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+                # DO NOT TOUCH
+                json.dump(
+                    dict(config["modeling"]["random_forest"].items()), fp)
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
-                "main",
+                os.path.join(hydra.utils.get_original_cwd(),
+                             'src', 'train_random_forest'),
+                'main',
                 parameters={
-                    "trainval_artifact": "trainval_data.csv:latest",
-                    "val_size": config["modeling"]["val_size"],
-                    "random_seed": config["modeling"]["random_seed"],
-                    "stratify_by": config["modeling"]["stratify_by"],
-                    "rf_config": rf_config,
-                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                    "output_artifact": "random_forest_export"
-                }
+                    'trainval_artifact': 'trainval_data.csv:latest',
+                    'val_size': config['modeling']['val_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                    'rf_config': rf_config,
+                    'max_tfidf_features': config['modeling']['max_tfidf_features'],
+                    'output_artifact': 'random_forest_export'
+                },
             )
 
         if "test_regression_model" in active_steps:
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/test_regression_model",
-                "main",
+                'main',
+                version='main',                
                 parameters={
-                    "mlflow_model": "random_forest_export:prod",
-                    "test_dataset": "test_data.csv:latest"
-                }
+                    'mlflow_model': 'random_forest_export:prod',
+                    'test_dataset': 'test_data.csv:latest'
+                },
             )
 
 
 if __name__ == "__main__":
-    go("basic_cleaning")
+    go()
