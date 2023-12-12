304ce8eac8f2d53155ea3ef2a318ddf22f7647a1
Huy Hoang <huy2000kudo@gmail.com>
2023-12-12 18:42:14 +0700

    change

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -44,8 +44,12 @@ def go(args):
     run.config.update(args)
 
     # Get the Random Forest configuration and update W&B
-    with open(args.rf_config) as fp:
+    # with open(args.rf_config) as fp:
+    #     rf_config = json.load(fp)
+    
+    with open("./rf_config.json") as fp:
         rf_config = json.load(fp)
+        
     run.config.update(rf_config)
 
     # Fix the random seed for the Random Forest, so we get reproducible results
